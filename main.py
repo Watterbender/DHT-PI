a068f524a5c1f270795359141d1e25650ccb2432
Watterbenders <kristjan.gersak@student.um.si>
2025-03-20 20:59:58 +0100

    Funkcija

--- main.py
+++ main.py
@@ -13,19 +13,26 @@ load_dotenv()
 DHT1 = adafruit_dht.DHT11(board.D5)
 DHT2 = adafruit_dht.DHT11(board.D6)
 
-#while True:
-try:
-    temperatura1 = DHT1.temperature
-    print("Temperatura={0:0.1f}ºC".format(temperatura1))
+def temperatura(DHT):
+    try:
+        temperatura1 = DHT.temperature
+        print("Temperatura={0:0.1f}ºC".format(temperatura1))
 
-except RuntimeError as err:
-    print(err.args[0])
-    #time.sleep(2.0)
-    #continue
+    except RuntimeError as err:
+        print(err.args[0])
+        #time.sleep(2.0)
+        #continue
 
-except Exception as err:
-    print("Velika napaka")
-    DHT1.exit()
-    raise err
+    except Exception as err:
+        print("Velika napaka")
+        DHT1.exit()
+        raise err
+
+
+#Majmune
+
+temperatura(DHT1)
+temperatura(DHT2)
+
 
 #time.sleep(3.0)
